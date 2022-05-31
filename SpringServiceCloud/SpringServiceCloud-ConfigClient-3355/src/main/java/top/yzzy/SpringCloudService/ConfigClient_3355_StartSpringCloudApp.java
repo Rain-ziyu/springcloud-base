8c761dbeb171ca0b5ce7875c3ee498cac9fd2315
Rain-ziyu <1874300301@qq.com>
2022-05-31 16:39:07 +0800

    读取git配置文件并环境切换

---
+++ SpringServiceCloud/SpringServiceCloud-ConfigClient-3355/src/main/java/top/yzzy/SpringCloudService/ConfigClient_3355_StartSpringCloudApp.java
@@ -0,0 +1,16 @@
+
+package top.yzzy.SpringCloudService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigClient_3355_StartSpringCloudApp
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ConfigClient_3355_StartSpringCloudApp.class, args);
+	}
+}
